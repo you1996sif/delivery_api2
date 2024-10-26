0160c881ad91d8e337f72429700188f48433264d
yousifmoufti996 <147701634+yousifmoufti996@users.noreply.github.com>
2024-10-26 17:28:53 +0300

    update

--- models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import stock_picking
-from . import state_model
-from . import district_model
-from . import res_partner
-#from . import api_db_middleware
-from . import prime
-from . import account_move
